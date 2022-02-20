713aeef5ef8914b1e70a7fe1ad6dc8e6485bcb6d
Maksim Tropnikov <makstropnikov@gmail.com>
2022-02-20 19:11:25 +0700

    Updates

--- src/components/Spinner/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-import spinner from "../../imgs/spinner.gif";
-import "./Spinner.scss";
-
-const Spinner = () => {
-  return <img src={spinner} alt='Loading...' className='spinner' />;
-};
-
-export default Spinner;
